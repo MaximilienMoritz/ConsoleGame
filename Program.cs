2b86bc9e52420b16eee67941fa72c1cc9bd8cc76
Void_Whaler <94693049+MaximilienMoritz@users.noreply.github.com>
2025-08-27 11:13:25 +0200

    Commit initial
    (bricolage)

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// // See https://aka.ms/new-console-template for more information
+//
+// Console.WriteLine("Hello, World!");
+//
+// if (0 < 2) {
+//     int b = 0;
+// } else {
+//     int a = 0;
+// }
+//
+// const int gridX = 20;
+// const int gridY = 5;
+//
+// string output = "";
+//
+// for (int i = 0; i < gridY; i++) {
+//     for (int j = 0; j < gridX; j++) {
+//         if (i % 2 == 1) {
+//             output += 'O';
+//             Console.BackgroundColor = ConsoleColor.Red;
+//             Console.ForegroundColor = ConsoleColor.Black;
+//         } else {
+//             output += 'U';
+//             Console.BackgroundColor = ConsoleColor.Green;
+//             Console.ForegroundColor = ConsoleColor.Black;
+//         }
+//     }
+//
+//     //Console.BackgroundColor = ConsoleColor.Cyan;
+//     Console.WriteLine(output);
+//
+//     output = "";
+// }
+//
+// Console.BackgroundColor = ConsoleColor.Black;
+// Console.WriteLine("a");
