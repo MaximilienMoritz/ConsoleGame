9b6a9b12764da9f93f4a3d0f35e5a583019c4b40
Void_Whaler <94693049+MaximilienMoritz@users.noreply.github.com>
2025-08-27 13:36:42 +0200

    Touches implémentées / Fuckery avec les lignes

---
+++ ScreenGridManager.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class ScreenGridManager {
+    public const int gridX = 20;
+    public const int gridY = 5;
+
+    public static int[,] grid;
+    
+    public static void drawGrid(int x, int y) {
+
+        grid = new int[gridX, gridY];
+       
+
+        string output = "";
+        for (int i = 0; i < gridY; i++) {
+            for (int j = 0; j < gridX; j++) {
+                if (i == x) {
+                    output += 'O';
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                } else {
+                    output += 'U';
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                
+                Console.Write(output);
+                
+            }
+            
+            
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            //Console.Write(output);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n");
+            
+            output = "";
+        }
+        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("a");
+    }
+}
